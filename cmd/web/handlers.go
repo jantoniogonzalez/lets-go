8de3adab63077ca30e8a13bbfa28a3a0dad83ae4
Juan <jgonzalez020801@gmail.com>
2024-06-12 23:27:05 -0700

    1. added logout

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -200,5 +200,12 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogout(w http.ResponseWriter, r *http.Request) {
-
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+	app.sessionManager.Put(r.Context(), "flash", "You have been logged out successfuly!")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -27,7 +27,7 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
 	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
-	router.Handler(http.MethodPost, "/user/logut", dynamic.ThenFunc(app.userLogout))
+	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogout))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, addSecurityHeaders)
 
